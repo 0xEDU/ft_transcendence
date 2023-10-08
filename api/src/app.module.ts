24504e0e73e04113cd3f3c553e69c95c7995d9d1
etachott <bfwaend@gmail.com>
2023-10-07 21:53:32 -0300

    feat: add backend base

---
+++ api/src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'postgres',
+      port: 5432,
+      password: 'root',
+      username: 'root',
+      entities: [],
+      database: 'postgres',
+      synchronize: true,
+      logging: true,
+    }),
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
