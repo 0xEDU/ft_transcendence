2dda8b718665ec0eaff85276884fa83a4d408dbe
Guiribei <guilherme_f_ribeiro@hotmail.com>
2023-11-10 01:52:54 -0300

    fix(pong): make paddle moviment smoother

--- transcendence-core/static/js/pong.js
+++ transcendence-core/static/js/pong.js
@@ -1,21 +1,22 @@
+let lastTime = 0;
 const canvas = document.getElementById('game');
 const context = canvas.getContext('2d');
-let canvasWidth = window.innerWidth * 0.8; // 80% of window width
-let canvasHeight = window.innerHeight * 0.8; // 80% of window height
+let canvasWidth = window.innerWidth * 0.8;
+let canvasHeight = window.innerHeight * 0.8;
 
-canvasWidth = Math.min(canvasWidth, 1536); // Max width
-canvasHeight = Math.min(canvasHeight, 864); // Max height
+canvasWidth = Math.min(canvasWidth, 1536);
+canvasHeight = Math.min(canvasHeight, 864);
 
 canvas.width = canvasWidth;
 canvas.height = canvasHeight;
 
 const grid = 15;
-const paddleHeight = grid * 5; // adjust based on canvas size
+const paddleHeight = grid * 6;
 const maxPaddleY = canvas.height - grid - paddleHeight;
-
-var paddleSpeed = 6;
-var ballSpeed = 5;
-const ballSpeedIncreaseFactor = 1.1;
+const paddleSpeed = 70;
+const ballSpeed = 4.7;
+const maxBallSpeed = 10;
+const ballSpeedIncreaseFactor = 1.10;
 
 const leftPaddle = {
   x: grid * 2,
@@ -43,25 +44,30 @@ const ball = {
   dy: -ballSpeed
 };
 
+const keyState = {
+  ArrowUp: false,
+  ArrowDown: false,
+  w: false,
+  s: false
+};
+
 function collides(obj1, obj2) {
   if (obj1.x < obj2.x + obj2.width &&
       obj1.x + obj1.width > obj2.x &&
       obj1.y < obj2.y + obj2.height &&
       obj1.y + obj1.height > obj2.y) {
       
-    // Determine where the ball hits the paddle (from -1 to 1)
     let hitPoint = (obj1.y + (obj1.height / 2)) - (obj2.y + (obj2.height / 2));
     hitPoint /= (obj2.height / 2);
 
-    // Calculate angle in radians
-    const angle = hitPoint * Math.PI / 4; // Max bounce angle is 45 degrees here
+    const angleRange = Math.PI / 4;
+    const angle = hitPoint * angleRange;
 
-    // Calculate the speed of the ball
-    const currentSpeed = Math.sqrt(obj1.dx * obj1.dx + obj1.dy * obj1.dy);
+    let currentSpeed = Math.sqrt(obj1.dx * obj1.dx + obj1.dy * obj1.dy);
+    currentSpeed = Math.min(currentSpeed * ballSpeedIncreaseFactor, maxBallSpeed);
 
-    // Adjust the direction of the ball
     obj1.dy = currentSpeed * Math.sin(angle);
-    obj1.dx = (obj1.dx > 0 ? -1 : 1) * currentSpeed * Math.cos(angle);
+    obj1.dx = -obj1.dx;
 
     return true;
   }
@@ -92,46 +98,53 @@ function drawScore() {
 function checkGameOver() {
   if (leftScore >= winningScore || rightScore >= winningScore) {
     gameOver = true;
-    
-    // Erase the middle line by drawing a smaller rectangle over it
-    // Adjust the y-coordinate and height to avoid the top and bottom borders
-    context.fillStyle = 'black'; // Use your background color
+    context.fillStyle = 'black';
     context.fillRect(canvas.width / 2 - grid / 2, grid, grid, canvas.height - grid * 2);
-
-    // Now draw the game over message
-    context.fillStyle = 'white'; // Text color
+    context.fillStyle = 'white';
     context.font = 'bold 50px Helvetica';
-    context.textAlign = 'center'; // Align text to the center
+    context.textAlign = 'center';
     context.fillText("Game Over!", canvas.width / 2, canvas.height / 2);
     context.fillText("Press Enter to restart", canvas.width / 2, canvas.height / 2 + 60);
-
-    // Reset text alignment if needed elsewhere
     context.textAlign = 'start';
   }
 }
 
-function loop() {
+function updatePaddleMovement(deltaTime) {
+  const movementScale = deltaTime * 0.01;
+
+  if (keyState.ArrowUp) rightPaddle.dy = -paddleSpeed;
+  else if (keyState.ArrowDown) rightPaddle.dy = paddleSpeed;
+  else rightPaddle.dy = 0;
+
+  if (keyState.w) leftPaddle.dy = -paddleSpeed;
+  else if (keyState.s) leftPaddle.dy = paddleSpeed;
+  else leftPaddle.dy = 0;
+
+  leftPaddle.y += leftPaddle.dy * movementScale;
+  rightPaddle.y += rightPaddle.dy * movementScale;
+
+  leftPaddle.y = Math.max(grid, Math.min(leftPaddle.y, maxPaddleY));
+  rightPaddle.y = Math.max(grid, Math.min(rightPaddle.y, maxPaddleY));
+}
+
+function loop(timestamp) {
   if (gameOver) {
     return;
   }
-  
+
+  const deltaTime = timestamp - lastTime;
+  lastTime = timestamp;
+
   requestAnimationFrame(loop);
   context.clearRect(0, 0, canvas.width, canvas.height);
-  
-  leftPaddle.y += leftPaddle.dy;
-  rightPaddle.y += rightPaddle.dy;
 
-  if (leftPaddle.y < grid) {
-    leftPaddle.y = grid;
-  } else if (leftPaddle.y > maxPaddleY) {
-    leftPaddle.y = maxPaddleY;
-  }
+  updatePaddleMovement(deltaTime);
 
-  if (rightPaddle.y < grid) {
-    rightPaddle.y = grid;
-  } else if (rightPaddle.y > maxPaddleY) {
-    rightPaddle.y = maxPaddleY;
-  }
+  if (leftPaddle.y < grid) leftPaddle.y = grid;
+  else if (leftPaddle.y > maxPaddleY) leftPaddle.y = maxPaddleY;
+
+  if (rightPaddle.y < grid) rightPaddle.y = grid;
+  else if (rightPaddle.y > maxPaddleY) rightPaddle.y = maxPaddleY;
 
   context.fillStyle = 'white';
   context.fillRect(leftPaddle.x, leftPaddle.y, leftPaddle.width, leftPaddle.height);
@@ -140,49 +153,35 @@ function loop() {
   ball.x += ball.dx;
   ball.y += ball.dy;
 
-  if (ball.y < grid) {
-    ball.y = grid;
+  if (ball.y < grid || ball.y + grid > canvas.height - grid) {
     ball.dy *= -1;
-    // Increase the speed after hitting the wall
-    ball.dx *= ballSpeedIncreaseFactor;
-    ball.dy *= ballSpeedIncreaseFactor;
-  } else if (ball.y + grid > canvas.height - grid) {
-    ball.y = canvas.height - grid * 2;
-    ball.dy *= -1;
-    // Increase the speed after hitting the wall
-    ball.dx *= ballSpeedIncreaseFactor;
-    ball.dy *= ballSpeedIncreaseFactor;
+    if (ball.y < grid) ball.y = grid;
+    else ball.y = canvas.height - grid * 2;
   }
 
   if (ball.x < 0 || ball.x > canvas.width) {
     if (!ball.resetting) {
       ball.resetting = true;
-  
-      // Update score
       if (ball.x < 0) rightScore++;
       else if (ball.x > canvas.width) leftScore++;
-  
+
       setTimeout(() => {
         ball.resetting = false;
-  
-        // Reset ball position
         ball.x = canvas.width / 2;
         ball.y = canvas.height / 2;
-  
-        // Randomly set the ball's direction on reset
-        const randomAngle = Math.random() * Math.PI/2 - Math.PI/4; // Random angle between -45 and 45 degrees
-        const direction = Math.random() < 0.5 ? 1 : -1; // Randomly choose between left and right
-  
+        const randomAngle = Math.random() * Math.PI/2 - Math.PI/4;
+        const direction = Math.random() < 0.5 ? 1 : -1;
         ball.dx = direction * ballSpeed * Math.cos(randomAngle);
         ball.dy = ballSpeed * Math.sin(randomAngle);
       }, 400);
     }
   }
 
-  if (collides(ball, leftPaddle)) {
-    ball.x = leftPaddle.x + leftPaddle.width;
-  } else if (collides(ball, rightPaddle)) {
-    ball.x = rightPaddle.x - ball.width;
+  if (collides(ball, leftPaddle) || collides(ball, rightPaddle)) {
+    // Ball speed influenced by paddle movement
+    let additionalSpeed = (leftPaddle.dy + rightPaddle.dy) / 4;
+    additionalSpeed = Math.max(Math.min(additionalSpeed, 1), -1);  // Limiting additional speed
+    ball.dy += additionalSpeed;
   }
 
   context.fillRect(ball.x, ball.y, ball.width, ball.height);
@@ -198,48 +197,38 @@ function loop() {
   checkGameOver();
 }
 
+// Key down event
 document.addEventListener('keydown', function(e) {
-  if (e.key === 'Enter' && gameOver) {
-    resetGame();
-    requestAnimationFrame(loop);
-  }
-  if (e.key === 'ArrowUp') {
-    rightPaddle.dy = -paddleSpeed;
-  } else if (e.key === 'ArrowDown') {
-    rightPaddle.dy = paddleSpeed;
-  }
-
-  if (e.key === 'w' || e.key === 'W') {
-    leftPaddle.dy = -paddleSpeed;
-  } else if (e.key === 's' || e.key === 'S') {
-    leftPaddle.dy = paddleSpeed;
-  }
-});
-
-document.addEventListener('keyup', function(e) {
-  if (e.key === 'ArrowUp' || e.key === 'ArrowDown') {
-    rightPaddle.dy = 0;
-  }
-
-  if (e.key === 'w' || e.key === 'W' || e.key === 's' || e.key === 'S') {
-    leftPaddle.dy = 0;
-  }
-});
+	if (e.key in keyState) {
+	  keyState[e.key] = true;
+	}
+  
+	// Check if 'Enter' is pressed and game is over
+	if (e.key === 'Enter' && gameOver) {
+	  resetGame();
+	  gameOver = false;  // Ensure the game is marked as not over
+	  requestAnimationFrame(loop);  // Restart the game loop
+	}
+  });
+  
+  // Key up event
+  document.addEventListener('keyup', function(e) {
+	if (e.key in keyState) {
+	  keyState[e.key] = false;
+	}
+  });
 
 window.addEventListener('resize', () => {
-    // Sync canvas drawing size with CSS size
-    const rect = canvas.getBoundingClientRect();
-    canvas.width = rect.width;
-    canvas.height = rect.height;
-
-    // Recalculate positions and sizes
-    leftPaddle.x = grid * 2;
-    leftPaddle.y = canvas.height / 2 - paddleHeight / 2;
-    rightPaddle.x = canvas.width - grid * 3;
-    rightPaddle.y = canvas.height / 2 - paddleHeight / 2;
-    ball.x = canvas.width / 2;
-    ball.y = canvas.height / 2;
-    maxPaddleY = canvas.height - grid - paddleHeight;
+  const rect = canvas.getBoundingClientRect();
+  canvas.width = rect.width;
+  canvas.height = rect.height;
+  leftPaddle.x = grid * 2;
+  leftPaddle.y = canvas.height / 2 - paddleHeight / 2;
+  rightPaddle.x = canvas.width - grid * 3;
+  rightPaddle.y = canvas.height / 2 - paddleHeight / 2;
+  ball.x = canvas.width / 2;
+  ball.y = canvas.height / 2;
+  maxPaddleY = canvas.height - grid - paddleHeight;
 });
 
 requestAnimationFrame(loop);
