5f5590310460c1e31ea14dedb9a245566cd5f950
etachott <bfwaend@gmail.com>
2023-11-12 20:16:35 -0300

    feat: add simple model

--- transcendence-core/app_users/models.py
+++ transcendence-core/app_users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    login_intra = models.CharField(max_length=20)
+    display_name = models.CharField(max_length=40)
+    avatar_image_url = models.CharField(max_length=200)
