9c6625dbabf5ec5770d4fac1c1ee069616b529c7
rodsmade <rodrigogregoldo@gmail.com>
2023-11-23 17:40:45 -0300

    big element emulating pane

--- static/js/core/pages/SPA.js
+++ static/js/core/pages/SPA.js
@@ -2,15 +2,15 @@ console.log(`inner height: ${window.innerHeight}`)
 
 document.addEventListener('scroll', function() {
     var scrollElement = document.getElementById('scrollElement');
-    var threshold_panel1 = window.innerHeight / 3; // Adjust as needed
-    var threshold_panel2 = window.innerHeight / 3 * 2; // Adjust as needed
+    var threshold_panel1 = window.innerHeight ; // Adjust as needed
+    var threshold_panel2 = window.innerHeight * 2; // Adjust as needed
 
     console.log(`current scroll: ${window.scrollY}`)
     if (window.scrollY < threshold_panel1) {
-        scrollElement.style.left = '50%';
+        scrollElement.style.left = '100%';
     } else if (window.scrollY > threshold_panel1 && window.scrollY < threshold_panel2) {
-        scrollElement.style.left = '40%';
+        scrollElement.style.left = '0%';
     } else if (window.scrollY > threshold_panel2) {
-        scrollElement.style.left = '30%';
+        scrollElement.style.left = '-100%';
     }
 });
