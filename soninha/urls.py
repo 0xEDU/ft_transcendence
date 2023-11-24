ba5d31f7492813061fd0919e37ac522b770f00b4
etachott <etachott@student.42sp.org.br>
2023-11-24 19:26:25 -0300

    chore: add logout endpoint

--- soninha/urls.py
+++ soninha/urls.py
@@ -6,4 +6,5 @@ app_name = 'auth'
 urlpatterns = [
     path('intra/', views.intra_view, name='intra'),
     path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
