1edf7f6b4b06401830c13d294ad2ccc2ca82f892
rodsmade <rodrigogregoldo@gmail.com>
2023-11-23 22:52:30 -0300

    first attempt at vertical scroll in two stages with horizontal panes SUCCESSFUL YAY

--- static/js/core/pages/SPA.js
+++ static/js/core/pages/SPA.js
@@ -1,16 +1,23 @@
 console.log(`inner height: ${window.innerHeight}`)
 
 document.addEventListener('scroll', function() {
-    var scrollElement = document.getElementById('scrollElement');
-    var threshold_panel1 = window.innerHeight ; // Adjust as needed
-    var threshold_panel2 = window.innerHeight * 2; // Adjust as needed
+    var firstPane = document.getElementById('first-pane');
+    var secondPane = document.getElementById('second-pane');
+    // var lastPane = document.getElementById('last-pane');
+
+    const viewportHeight = window.innerHeight;
+    const viewportWidth = window.innerWidth;
+
+    var thresholdOne = window.innerHeight;
+    // var thresholdTwo = window.innerHeight * 2;
 
     console.log(`current scroll: ${window.scrollY}`)
-    if (window.scrollY < threshold_panel1) {
-        scrollElement.style.left = '100%';
-    } else if (window.scrollY > threshold_panel1 && window.scrollY < threshold_panel2) {
-        scrollElement.style.left = '0%';
-    } else if (window.scrollY > threshold_panel2) {
-        scrollElement.style.left = '-100%';
+    if (window.scrollY <= thresholdOne) {
+        secondPane.style.left = 0 + 'px';
+        secondPane.style.top = viewportHeight + 'px';
+    } else if (window.scrollY > thresholdOne) {
+        secondPane.style.left = ((((2 * viewportHeight - window.scrollY) / viewportHeight) - 1) * 100) + '%';
+        secondPane.style.top = window.scrollY + 'px';
+        console.log(`calculated top: ${calculated_top}`)
     }
 });
