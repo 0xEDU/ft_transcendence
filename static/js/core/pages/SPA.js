9187baefedc75dbed63dfb583366c751f8898583
rodsmade <rodrigogregoldo@gmail.com>
2023-11-25 12:02:08 -0300

    implemented fourth panel sliding in

--- static/js/core/pages/SPA.js
+++ static/js/core/pages/SPA.js
@@ -3,21 +3,28 @@ console.log(`inner height: ${window.innerHeight}`)
 document.addEventListener('scroll', function() {
     var firstPane = document.getElementById('first-pane');
     var secondPane = document.getElementById('second-pane');
-    // var lastPane = document.getElementById('last-pane');
+    var lastPane = document.getElementById('last-pane');
 
     const viewportHeight = window.innerHeight;
     const viewportWidth = window.innerWidth;
 
     var thresholdOne = window.innerHeight;
-    // var thresholdTwo = window.innerHeight * 2;
+    var thresholdTwo = window.innerHeight * 2;
 
     console.log(`current scroll: ${window.scrollY}`)
-    if (window.scrollY <= thresholdOne) {
+    if (window.scrollY <= thresholdOne)
+    {
         secondPane.style.left = 0 + 'px';
         secondPane.style.top = viewportHeight + 'px';
-    } else if (window.scrollY > thresholdOne) {
+    }
+    else if (window.scrollY > thresholdOne && window.scrollY <= thresholdTwo) // 1 x VH
+    {
         secondPane.style.left = ((((2 * viewportHeight - window.scrollY) / viewportHeight) - 1) * 100) + '%';
         secondPane.style.top = window.scrollY + 'px';
-        console.log(`calculated top: ${calculated_top}`)
+        lastPane.style.top = window.scrollY + viewportHeight + 'px';
+    }
+    else if (window.scrollY > thresholdTwo) // 2 x VH
+    {
+        secondPane.style.left = - viewportWidth + 'px';
     }
 });
