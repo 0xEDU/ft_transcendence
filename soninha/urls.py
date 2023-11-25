fbff785a322a3411b283c3668dfaccb5292da095
etachott <etachott@student.42sp.org.br>
2023-11-25 02:50:50 -0300

    wip: working on proper login in the back end

--- soninha/urls.py
+++ soninha/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('intra/', views.intra_view, name='intra'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path('user/', views.LoggedUserView.as_view(), name='user')
 ]
