09dd1ca27f0b57c4677dc71f75d4f3d223f95a24
Guilherme Ribeiro <guilherme_f_ribeiro@hotmail.com>
2023-12-06 21:49:13 -0300

    feat(home): allow scroll on button

---
+++ static/js/move-svg-circle.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var homeSection = document.getElementById('home');
+    window.scrollTo({
+        top: homeSection.offsetTop,
+        behavior: 'smooth'
+    });
+
+    var powerCircle = document.querySelector('#power-button-svg circle');
+    powerCircle.addEventListener('click', function() {
+        var currentY = parseFloat(powerCircle.getAttribute('cy'));
+        var newY = (currentY === 147.54) ? 54.54 : 147.54;
+        
+        powerCircle.setAttribute('cy', newY.toString());
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+});
+
