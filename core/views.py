c4b63a30373e2ea07293734b3c4496293d51fafa
etachott <bfwaend@gmail.com>
2024-01-05 18:41:22 -0300

    feat: finish user info frontend

--- core/views.py
+++ core/views.py
@@ -4,8 +4,14 @@ from soninha.views import UserTemplateView
 import os
 
 def home(request):
+    intra_uid = os.getenv('INTRA_UID') 
+    redirect_uri = os.getenv('TRANSCENDENCE_IP')
     context = {
-        "redirect_url": os.getenv('INTRA_ENDPOINT') + "/oauth/authorize?client_id=" + os.getenv('INTRA_UID') + "&redirect_uri=http%3A%2F%2F10.11.7.3%3A8000%2Fauth%2Flogin%2F&response_type=code" ,
+        "redirect_url": os.getenv('INTRA_ENDPOINT') + "/oauth/authorize?client_id=" + intra_uid + "&redirect_uri=http%3A%2F%2F" + redirect_uri + "%3A8000%2Fauth%2Flogin%2F&response_type=code" ,
+        "user_helps": 120,
+        "user_ball_distance": "10 mi",
+        "user_time_played": "15h",
+        "user_number_of_friends": 12,
     }
     session = request.session
     if "user_id" in session and session["user_id"] != "":
