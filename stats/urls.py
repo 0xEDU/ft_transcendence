00c41e03745ac7cc9917215af5f07bb45b02e330
etachott <bfwaend@gmail.com>
2024-01-08 14:12:22 -0300

    feat(stats): add matches history as CBV

--- stats/urls.py
+++ stats/urls.py
@@ -1,6 +1,8 @@
+"""URLs for the stats app."""
 from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('matches-history/', views.matchesHistory, name='matchHistory'),
+    path('matches-history/', views.MatchesHistoryTemplate.as_view(),
+         name='matchHistory'),
 ]
--- stats/views.py
+++ stats/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+"""Views for the stats app."""
+from django.views.generic import TemplateView
 
-# Create your views here.
-def matchesHistory(request):
-	return render(request, 'stats/matchHistory.html')
+
+class MatchesHistoryTemplate(TemplateView):
+    """Returns the matches history template."""
+
+    template_name = "stats/components/matches-history.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
