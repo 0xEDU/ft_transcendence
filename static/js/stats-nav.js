da678ed48d90e6aba78bce2aa56fd6815e09a05a
etachott <bfwaend@gmail.com>
2024-01-08 13:49:08 -0300

    feat(stats): highlight selected page properly

--- static/js/stats-nav.js
+++ static/js/stats-nav.js
@@ -1,5 +1,9 @@
-const currentStatPage = "matchesHistoryText"
+let currentStatPage = "matchesHistoryText"
 const statPages = ["matchesHistoryText", "tournamentsText", "userStatsText"]
+
+function removeHighlight() {
+	document.getElementById(currentStatPage).style.textDecoration = "none";
+}
 
 function highlightCurrentPage() {
 	document.getElementById(currentStatPage).style.textDecoration = "underline";
@@ -9,9 +13,13 @@ document.addEventListener("DOMContentLoaded", function() {
 	const statsNav = document.getElementById("statsNav");
 	highlightCurrentPage();
 
+	// Highlight the current 'page' when clicked
 	statsNav.addEventListener("click", function(event) {
-		const 
-		highlightCurrentPage();
+		if (event.target.id !== currentStatPage && statPages.includes(event.target.id)) {
+			removeHighlight();
+			currentStatPage = event.target.id;
+			highlightCurrentPage();
+		}
 	})
 })
 
