4386c2bfd440d85655dd7c1691d9a88440fd5f8c
etachott <bfwaend@gmail.com>
2024-01-10 21:26:40 -0300

    feat: add stats tournament view

--- stats/urls.py
+++ stats/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('matches-history/', views.MatchesHistoryTemplate.as_view(),
+    path('matches-history/', views.MatchesHistoryTemplateView.as_view(),
          name='matchHistory'),
+    path('tournaments/', views.TournamentsTemplateView.as_view(),
+         name='tournaments'),
 ]
--- stats/views.py
+++ stats/views.py
@@ -2,10 +2,20 @@
 from django.views.generic import TemplateView
 
 
-class MatchesHistoryTemplate(TemplateView):
+class MatchesHistoryTemplateView(TemplateView):
     """Returns the matches history template."""
 
     template_name = "stats/components/matches-history.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+
+class TournamentsTemplateView(TemplateView):
+    """Returns the tournaments template."""
+
+    template_name = "stats/components/tournaments.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
