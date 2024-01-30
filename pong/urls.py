82a5259722cd4381c91a9b3cef1828af7eb8f33e
etachott <bfwaend@gmail.com>
2024-01-29 21:14:45 -0300

    feat(urls): add GameFormView to urls Co-authored-by: Guilherme Ribeiro <Guiribei@users.noreply.github.com>

--- pong/urls.py
+++ pong/urls.py
@@ -1,10 +1,11 @@
 """URLs for the pong app."""
 from django.urls import path
-from .views import home_view, GameView, MatchView
+from .views import home_view, GameView, MatchView, GameFormView
 
 app_name = "pong" # pylint: disable=invalid-name
 urlpatterns = [
     path('', home_view, name='home'),
     path('game', GameView.as_view(), name='game'),
     path('match', MatchView.as_view(), name='match'),
+    path('form', GameFormView.as_view(), name='form'),
 ]
