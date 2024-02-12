8fce267cea845b140d2402a4aa4de683a6192f23
etachott <bfwaend@gmail.com>
2024-02-12 15:05:13 -0300

    refactor: clean up lobby js

--- static/js/play-button.js
+++ static/js/play-button.js
@@ -3,18 +3,63 @@ import appendElement from "./tinyDOM/appendElement.js";
 import deleteElement from "./tinyDOM/deleteElement.js";
 import hasElement from "./tinyDOM/hasElement.js";
 
+class ModalObj {
+    constructor(form, modalElement, modalInstance, playButtonSvg, playButtonDiv, fourPlayersRadio, redCircle) {
+        this.form = form;
+        this.modalElement = modalElement;
+        this.modalInstance = modalInstance;
+        this.playButtonSvg = playButtonSvg;
+        this.playButtonDiv = playButtonDiv;
+        this.fourPlayersRadio = fourPlayersRadio;
+        this.redCircle = redCircle;
+        
+        const buttonMouseDown = () => { this.redCircle.setAttribute('cy', '68'); };
+        const buttonMouseUp = () => { this.redCircle.setAttribute('cy', '58'); };
+        this.playButtonSvg.addEventListener('mousedown', buttonMouseDown);
+        this.playButtonSvg.addEventListener('mouseup', buttonMouseUp);
+    }
+}
+
+function curry(f) {
+    return function(a) {
+        return function(b) {
+            return f(a, b);
+        }
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function() {
-    const playButtonSvg = document.getElementById('playButtonSvg');
-    const redCircle = document.getElementById('redCircle');
-    const modalElement = document.getElementById('singleMatchModal');
-    const singleMatchForm = document.getElementById('singleMatchForm');
+    const smObj = new ModalObj(
+        document.getElementById('singleMatchForm'),
+        document.getElementById('singleMatchModal'),
+        new bootstrap.Modal(document.getElementById('singleMatchModal'), {}),
+        document.getElementById('smPlayButtonSvg'),
+        document.getElementById('smPlayButtonDiv'),
+        document.getElementById('smFourPlayersRadio'),
+        document.getElementById('smRedCircle'),
+    )
+    
+    const tObj = new ModalObj(
+        document.getElementById('tournamentForm'),
+        document.getElementById('tournamentModal'),
+        new bootstrap.Modal(document.getElementById('tournamentModal'), {}),
+        document.getElementById('tPlayButtonSvg'),
+        document.getElementById('tPlayButtonDiv'),
+        document.getElementById('tFourPlayersRadio'),
+        document.getElementById('tRedCircle'),
+    )
+
     const twoPlayersRadio = document.getElementById('twoPlayersRadio');
-    const fourPlayersRadioSM = document.getElementById('fourPlayersRadioSM');
-    const fourPlayersRadioT = document.getElementById('fourPlayersRadioT');
     const eightPlayersRadio = document.getElementById('eightPlayersRadio');
+
     const secondPlayerInput = document.getElementById('secondPlayer');
     const thirdPlayerInput = document.getElementById('thirdPlayer');
     const fourthPlayerInput = document.getElementById('fourthPlayer');
+    const fifthPlayerInput = document.getElementById('fifthPlayer');
+    const sixthPlayerInput = document.getElementById('sixthPlayer');
+    const seventhPlayerInput = document.getElementById('seventhPlayer');
+    const eighthPlayerInput = document.getElementById('eighthPlayer');
+
     const player3And4Div = document.getElementById('player3And4Div');
     const player5And6Div = document.getElementById('player5And6Div');
     const player7And8Div = document.getElementById('player7And8Div');
@@ -28,14 +73,18 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     })
 
-    fourPlayersRadioSM.addEventListener('change', function() {
+    smObj.fourPlayersRadio.addEventListener('change', function() {
         if (fourPlayersRadioSM.checked) {
             player3And4Div.classList.remove('visually-hidden');
         } 
     })
     
-    fourPlayersRadioT.addEventListener('change', function() {
+    tObj.fourPlayersRadio.addEventListener('change', function() {
         if (fourPlayersRadioT.checked) {
+            fifthPlayerInput.value = '';
+            sixthPlayerInput.value = '';
+            seventhPlayerInput.value = '';
+            eighthPlayerInput.value = '';
             player5And6Div.classList.add('visually-hidden');
             player7And8Div.classList.add('visually-hidden');
         } 
@@ -48,19 +97,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     })
 
-    const modalInstance = new bootstrap.Modal(modalElement, {
-        // options, if any
-    });
-
-    playButtonSvg.addEventListener('mousedown', function() {
-        redCircle.setAttribute('cy', '68');
-    });
-
-    playButtonSvg.addEventListener('mouseup', function() {
-        redCircle.setAttribute('cy', '58');
-    });
-
-    singleMatchForm.addEventListener("submit", (event) => {
+    function handleFormSubmit(modalObj, event) {
         event.preventDefault();
         const url = "/pong/form";
         const formData = new FormData(event.target);
@@ -73,10 +110,10 @@ document.addEventListener('DOMContentLoaded', function() {
             if (!response.ok) {
                 return Promise.reject(response);
             }
-            if (hasElement("playButtonDiv", "playerNotFound")) {
+            if (hasElement(modalObj.playButtonDiv.id, "playerNotFound")) {
                 deleteElement("playerNotFound");
             }
-            modalInstance.hide();
+            modalObj.modalInstance.hide();
             secondPlayerInput.value = '';
             thirdPlayerInput.value = '';
             fourthPlayerInput.value = '';
@@ -88,10 +125,13 @@ document.addEventListener('DOMContentLoaded', function() {
         })
         .catch(error => {
             error.text().then(errorBody => {
-                if (!hasElement("playButtonDiv", "playerNotFound")) {
-                    appendElement("playButtonSvg", errorBody);
+                if (!hasElement(modalObj.playButtonDiv.id, "playerNotFound")) {
+                    appendElement(modalObj.playButtonSvg.id, errorBody);
                 }
             });
         });
-    });
+    }       
+
+    smObj.form.addEventListener("submit", curry(handleFormSubmit)(smObj));
+    tObj.form.addEventListener("submit", curry(handleFormSubmit)(tObj));
 });
