c21b30a7a4a2f9076f409efd7f5f55294955624a
Guilherme Ribeiro <guilherme_f_ribeiro@hotmail.com>
2024-02-25 19:28:19 -0300

    feat: change TemplateView game
    Co-authored-by: Eduardo Tachotte <etachott@student.42sp.org.br>

--- pong/urls.py
+++ pong/urls.py
@@ -1,11 +1,11 @@
 """URLs for the pong app."""
 from django.urls import path
-from .views import home_view, GameView, MatchView, GameFormView
+from .views import home_view, GameTemplateView, MatchView, GameFormView
 
 app_name = "pong" # pylint: disable=invalid-name
 urlpatterns = [
     path('', home_view, name='home'),
-    path('game', GameView.as_view(), name='game'),
+    path('game', GameTemplateView.as_view(), name='game'),
     path('match', MatchView.as_view(), name='match'),
     path('form', GameFormView.as_view(), name='form'),
 ]
--- pong/views.py
+++ pong/views.py
@@ -2,7 +2,7 @@
 import json
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import View
+from django.views.generic import View, TemplateView
 from soninha.models import User
 from pong.models import Match, Score
 
@@ -73,45 +73,36 @@ class MatchView(View):
             return HttpResponse('Something went wrong in the Match View')
 
 
-class GameView(View):
+class GameTemplateView(TemplateView):
     """
     This view is called when the game starts, it get/create users,
     create a match and a score, then pass it as context to our template
     """
+    template_name = "arena/components/game.html"
 
-    def get(self, request, *args, **kwargs):
-        """Get method."""
+    def get_context_data(self, **kwargs):
+        """Returns the context data."""
 
-        player1 = User.objects.get(login_intra="etachott")
-        player2 = User.objects.get(login_intra="roaraujo")
-        match = Match.objects.create()
-        Score.objects.create(player=player1, match=match, score=0)
-        Score.objects.create(player=player2, match=match, score=0)
-        match.players.add(player1, player2)
-        context = {
-            "records": [],
-            "player1": player1.display_name,
-            "player2": player2.display_name,
-            "match_id": match.id
-        }
-        return render(request, 'pong/pages/game.html', context)
+        context = super().get_context_data(**kwargs)
+        return context
+    
+    # def get(self, request, *args, **kwargs):
+    #     """Get method."""
 
-    def post(self, request, *args, **kwargs):
-        """Post method."""
+    #     player1 = User.objects.get(login_intra="etachott")
+    #     player2 = User.objects.get(login_intra="roaraujo")
+    #     match = Match.objects.create()
+    #     Score.objects.create(player=player1, match=match, score=0)
+    #     Score.objects.create(player=player2, match=match, score=0)
+    #     match.players.add(player1, player2)
+    #     context = {
+    #         "records": [],
+    #         "player1": player1.display_name,
+    #         "player2": player2.display_name,
+    #         "match_id": match.id
+    #     }
+    #     return render(request, 'pong/pages/game.html', context)
 
-        player1, _ = User.objects.get(login_intra=request.POST['player1'])
-        player2, _ = User.objects.get(login_intra=request.POST['player2'])
-        match = Match.objects.create()
-        Score.objects.create(player=player1, match=match, score=0)
-        Score.objects.create(player=player2, match=match, score=0)
-        match.players.add(player1, player2)
-        context = {
-            "records": [],
-            "player1": request.POST['player1'],
-            "player2": request.POST['player2'],
-            "match_id": match.id
-        }
-        return render(request, 'pong/pages/game.html', context)
 
 class GameFormView(View):
     """
@@ -129,4 +120,5 @@ class GameFormView(View):
             return render(request, 'components/errors/player_not_registered.html', {
                 'error_message': "Invalid user"
             }, status=400)
-        return HttpResponse("")
+        request.method = "GET"
+        return GameTemplateView.as_view()(request)
