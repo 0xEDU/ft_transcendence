251a0f1dd75cde5a24a69cffb467776aedd7f5fa
rodsmade <rodrigogregoldo@gmail.com>
2024-02-26 19:36:25 -0300

    feat: blocking files that are not jpeg or png

--- static/js/profile.js
+++ static/js/profile.js
@@ -1,11 +1,33 @@
-document.getElementById('profilePicEditionInput').addEventListener('change', function(event) {
+document.getElementById('profilePicEditionInput').addEventListener('change', function (event) {
+    // When the user selects a new picture from the file system, 
     var file = event.target.files[0];
     var reader = new FileReader();
+    var currentProfilePicDiv = document.getElementById('currentProfilePicture');
+    var oldPicUrl = currentProfilePicDiv.style.backgroundImage;
+    console.log(oldPicUrl)
 
-    reader.onload = function(event) {
-        var innermostDiv = document.getElementById('currentProfilePicture');
-        innermostDiv.style.backgroundImage = `url(${event.target.result})`
-    };
-
-    reader.readAsDataURL(file);
+    if (file && file.type.startsWith('image/')) {
+        // Check if the selected file is in a common image format (JPG, PNG)
+        if (file.type === 'image/jpeg' || file.type === 'image/png') {
+            reader.onload = function (event) {
+                // Update preview with newly selected photo
+                currentProfilePicDiv.style.backgroundImage = `url(${event.target.result})`
+            };
+            
+            reader.readAsDataURL(file);
+        } else {
+            // Display an error message if the file format is not supported
+            alert('Please select a file in JPG or PNG format.');
+            // Clear the file input to prevent submission
+            currentProfilePicDiv.style.backgroundImage = oldPicUrl;
+            event.target.value = '';
+        }
+    } else {
+        // Display an error message if the selected file is not an image
+        alert('Please select an image file.');
+        // Clear the file input to prevent submission
+        currentProfilePicDiv.style.backgroundImage = oldPicUrl;
+        event.target.value = '';
+    }
 });
+
