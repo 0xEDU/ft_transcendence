8c24dd6f6d137d1f2619ff060f31ec8dee34f140
rodsmade <rodrigogregoldo@gmail.com>
2024-02-28 11:50:26 -0300

    WIP: saving newly uploaded pfp to filesystem and database

--- soninha/views.py
+++ soninha/views.py
@@ -1,10 +1,11 @@
 """Views for the soninha app."""
 
 # Python Std Libs
-import os
-import json
-import requests
 from http import HTTPStatus
+import json
+import os
+import requests
+import uuid
 
 # Our imports
 from soninha.models import User
@@ -12,7 +13,7 @@ from soninha.models import User
 # Django's imports
 from django.conf import settings
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import redirect
+from django.shortcuts import redirect, get_object_or_404
 from django.views import View
 from django.views.generic import TemplateView
 
@@ -28,7 +29,7 @@ class UserTemplateView(TemplateView):
             user = User.objects.get(pk=session["user_id"])
             context["h1_margin"] = "mb-4"
             context["h1_text"] = "Logged as " + user.login_intra
-            context["user_image"] = user.avatar_image_url
+            context["user_image"] = user.intra_cdn_profile_picture_url
             context["anchor_function"] = 'id=logoutButton'
             context["anchor_text"] = "Logout"
             return context
@@ -74,7 +75,7 @@ class LoginView(View):
         if not pfp_intra:
             pfp_intra = "/static/images/default_user_image.svg"
         new_user, _ = User.objects.get_or_create(
-            display_name=login_intra, login_intra=login_intra, avatar_image_url=pfp_intra)
+            display_name=login_intra, login_intra=login_intra, intra_cdn_profile_picture_url=pfp_intra)
         request.session["user_id"] = new_user.id
         return redirect('/')  # This will return the html
 
@@ -91,17 +92,32 @@ class LogoutView(View):
 def update_profile_picture(request):
     if request.method == 'POST':
         uploaded_file = request.FILES['profilePicture']
+        user = get_object_or_404(User, pk=request.session["user_id"])
+
+        # Delete the old profile picture file if it exists
+        if user.profile_picture and os.path.exists(str(user.profile_picture)):
+            os.remove(str(user.profile_picture))
+
+        # Generate a unique filename for the uploaded file
+        filename = f"{uuid.uuid4().hex}{os.path.splitext(uploaded_file.name)[1]}"
+
         # Define the directory where you want to save the uploaded files
         upload_dir = os.path.join(settings.MEDIA_ROOT, 'profile_pictures')
         if not os.path.exists(upload_dir):
             os.makedirs(upload_dir)
-        # Construct the file path
-        file_path = os.path.join(upload_dir, uploaded_file.name)
+        
+        # Construct the file path with the unique filename
+        file_path = os.path.join(upload_dir, filename)
+
         # Save the file to the filesystem
         with open(file_path, 'wb') as destination:
             for chunk in uploaded_file.chunks():
                 destination.write(chunk)
-        # Now, you have the file saved at file_path
-        # You can update the user's profile picture attribute with this file path
-        return JsonResponse({"status": "OK", "file_path": file_path})
-    return JsonResponse({"status":HTTPStatus.METHOD_NOT_ALLOWED})
+
+        # Update the user's profile picture attribute with the file path
+        user.profile_picture = file_path
+        user.save()
+
+        return JsonResponse({"status": "OK", "file_path": user.profile_picture.url})
+    return JsonResponse({"status": HTTPStatus.METHOD_NOT_ALLOWED})
+
