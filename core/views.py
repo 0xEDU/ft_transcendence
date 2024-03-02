674297bc9b41024f1af23c9d1cbe2b801f71e52d
rodsmade <rodrigogregoldo@gmail.com>
2024-03-02 13:34:54 -0300

    refactor: tidied up code for clarity and maintainability

--- core/views.py
+++ core/views.py
@@ -7,39 +7,49 @@ from soninha.models import User,Achievements
 
 # Django's imports
 from django.shortcuts import render
+from django.views import View
 
-def home(request):
+
+class IndexView(View):
     """Renders the home page."""
 
-    intra_uid = os.getenv('INTRA_UID')
-    redirect_uri = os.getenv('TRANSCENDENCE_IP')
-    intra_endpoint = os.getenv('INTRA_ENDPOINT')
-    protocol = os.getenv('TRANSCENDENCE_PROTOCOL')
-    context = {
-        "redirect_url": intra_endpoint + "/oauth/authorize?client_id=" + intra_uid + "&redirect_uri=" + protocol + "%3A%2F%2F" + redirect_uri + "%3A8000%2Fauth%2Flogin%2F&response_type=code",
-        "user_helps": 120,
-        "user_ball_distance": "10 mi",
-        "user_time_played": "15h",
-        "user_number_of_friends": 12,
-    }
-    session = request.session
-    user_is_logged_in = False
-    if "user_id" in session and session["user_id"] != "":
-        user = User.objects.get(pk=session["user_id"])
-        context["user_login"] = user.login_intra
-        context["user_display_name"] = user.display_name
-        context["user_image"] = user.profile_picture.url if user.profile_picture else user.intra_cdn_profile_picture_url
-        user_is_logged_in = True
-        ############## ACHIEVEMENTS VALUES
-        achievement, _ = Achievements.objects.get_or_create(user=user)
-        context["achievement_ball_distance"] = achievement.ball_distance
-        context["achievement_friends_count"] = achievement.friends_count
-        context["achievement_hours_played"] = achievement.hours_played
-        context["achievement_matches_classic"] = achievement.matches_classic
-        context["achievement_matches_coop"] = achievement.matches_coop
-        context["achievement_matches_won"] = achievement.matches_won
-        ############### END
-    else:
-        context["h1_text"] = "You are not logged in"
-    context["user_is_logged_in"] = user_is_logged_in
-    return render(request, 'index.html', context)
+    def get(self, request, *args, **kwargs):
+        # Get context
+        # Build redirect url
+        intra_uid = os.getenv('INTRA_UID')
+        redirect_uri = os.getenv('TRANSCENDENCE_IP')
+        intra_endpoint = os.getenv('INTRA_ENDPOINT')
+        protocol = os.getenv('TRANSCENDENCE_PROTOCOL')
+        redirect_url = intra_endpoint + \
+                        "/oauth/authorize?client_id=" + intra_uid + \
+                        "&redirect_uri=" + protocol + "%3A%2F%2F" + \
+                        redirect_uri + "%3A8000%2Fauth%2Flogin%2F&response_type=code"
+
+        context = {
+            "redirect_url": redirect_url,
+            "user_helps": 120,
+            "user_ball_distance": "10 mi",
+            "user_time_played": "15h",
+            "user_number_of_friends": 12,
+        }
+
+        # Check if there is a logged in user
+        session = request.session
+        user_is_logged_in = "user_id" in session and session["user_id"] != ""
+        context["user_is_logged_in"] = user_is_logged_in
+        if user_is_logged_in:
+            # Get user info
+            user = User.objects.get(pk=session["user_id"])
+            context["user_login"] = user.login_intra
+            context["user_display_name"] = user.display_name
+            context["user_image"] = user.profile_picture.url if user.profile_picture else user.intra_cdn_profile_picture_url
+            # Get user achievements info
+            achievement = Achievements.objects.get(user=user)
+            context["achievement_ball_distance"] = achievement.ball_distance
+            context["achievement_friends_count"] = achievement.friends_count
+            context["achievement_hours_played"] = achievement.hours_played
+            context["achievement_matches_classic"] = achievement.matches_classic
+            context["achievement_matches_coop"] = achievement.matches_coop
+            context["achievement_matches_won"] = achievement.matches_won
+
+        return render(request, 'index.html', context)
