dbd9b16e5804a306131ed41c2c830e13273f2254
etachott <bfwaend@gmail.com>
2024-03-13 12:53:31 -0300

    downgrade solc version to 0.8.13

--- truffle/tournament/contracts/Tournaments.sol
+++ truffle/tournament/contracts/Tournaments.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
-// compiler version must be greater than or equal to 0.8.23 and less than 0.9.0
-pragma solidity ^0.8.21;
+pragma solidity ^0.8.13;
 
 contract Tournaments {
     struct Match {
