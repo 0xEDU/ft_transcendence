05d96897f9bb352438f3b94f53f4a15670820b7f
Guiribei <guilherme_f_ribeiro@hotmail.com>
2024-03-13 11:06:33 -0300

    fix: changing to a non arrow function fecthAndUpdateFriends

--- static/js/mates-along.js
+++ static/js/mates-along.js
@@ -1,26 +1,26 @@
 import swapInnerHTMLOfElement from "./tinyDOM/swapInnerHTMLOfElement.js";
 
-document.addEventListener('DOMContentLoaded', () => {
-	fetch('/get-friends/')
-	.then(response => response.json())
-	.then(data => {
-		let friendsHTML = '';
-		data.forEach(friend => {
-			const onlineStatus = friend.online ? '🟢' : '🔴';
-			friendsHTML += `
-			  <div class="mate-container">
-				<img class="matesImages me-3 mt-3"
-					 src="${friend.image_url}" 
-					 alt="${friend.username}'s Photo" 
-					 title="${friend.username}">
-				<span class="online-status">${onlineStatus}</span>
-			  </div>
-			`;
-		  });
+export default function fetchAndUpdateFriends() {
+    fetch('/get-friends/')
+    .then(response => response.json())
+    .then(data => {
+        let friendsHTML = '';
+        data.forEach(function(friend) {
+            const onlineStatus = friend.online ? '🟢' : '🔴';
+            friendsHTML += `
+              <div class="mate-container">
+                <img class="matesImages me-3 mt-3"
+                     src="${friend.image_url}" 
+                     alt="${friend.username}'s Photo" 
+                     title="${friend.username}">
+                <span class="online-status">${onlineStatus}</span>
+              </div>
+            `;
+          });
 
-		swapInnerHTMLOfElement('friendsContainer', friendsHTML);
-	})
-	.catch(error => {
-		console.error('Error loading friends:', error);
-	});
-});
+        swapInnerHTMLOfElement('friendsContainer', friendsHTML);
+    })
+    .catch(function(error) {
+        console.error('Error loading friends:', error);
+    });
+}
