582f7ab2a1bdaf9dba4ce4b198de1430ffdced72
etachott <bfwaend@gmail.com>
2024-03-14 00:49:46 -0300

    balls bruh

--- static/js/mates-along.js
+++ static/js/mates-along.js
@@ -2,13 +2,21 @@ import swapInnerHTMLOfElement from "./tinyDOM/swapInnerHTMLOfElement.js";
 
 export default function fetchAndUpdateFriends() {
     fetch('/get-friends/')
-    .then(response => response.json())
-    .then(data => {
-        let friendsHTML = '';
-        data.forEach(function(friend) {
-            const onlineStatus = friend.online ? '🟢' : '🔴';
-            friendsHTML += `
+        .then(response => response.json())
+        .then(data => {
+            let friendsHTML = '';
+            data.forEach(function(friend) {
+                let onlineStatus = friend.online ? '🟢' : '🔴';
+                const isChrome = navigator.userAgent.includes("Chrome") && navigator.vendor.includes("Google Inc");
+                let chromeBall = ""
+                if (isChrome) {
+                    onlineStatus = ""
+                    chromeBall = friend.online ? "<div class=\"green-ball\"></div>"
+                        : "<div class=\"red-ball\"></div>"
+                }
+                friendsHTML += `
               <div class="mate-container">
+                ${chromeBall}
                 <img class="matesImages me-3 mt-3"
                      src="${friend.image_url}" 
                      alt="${friend.username}'s Photo" 
@@ -16,11 +24,11 @@ export default function fetchAndUpdateFriends() {
                 <span class="online-status">${onlineStatus}</span>
               </div>
             `;
-          });
+            });
 
-        swapInnerHTMLOfElement('friendsContainer', friendsHTML);
-    })
-    .catch(function(error) {
-        console.error('Error loading friends:', error);
-    });
+            swapInnerHTMLOfElement('friendsContainer', friendsHTML);
+        })
+        .catch(function(error) {
+            console.error('Error loading friends:', error);
+        });
 }
